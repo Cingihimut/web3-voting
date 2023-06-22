f089200ff39dcd31840f2d91425e21b0ce9b1d3d
Muhammad Aris <mhdarisid@gmail.com>
2023-06-23 00:30:38 +0700

    Get Candidates From Smart Contract: fix vercel deployment

--- src/app/utils/fetchCandidateData.js
+++ src/app/utils/fetchCandidateData.js
@@ -1,6 +1,6 @@
 // import web3 from "web3";
 import { Contract, Web3 } from "web3";
-import contractABI from "../../../build/contracts/TestVote"; // Import ABI dari smart contract
+import contractABI from "../../contracts/TestVote"; // Import ABI dari smart contract
 const web3 = new Web3("http://localhost:9545");
 const contractAddress = "0x004cB9e3e476460a63F14F5E5212C7114d0A0aB6"; // Alamat kontrak smart contract
 const contract = new Contract(contractABI.abi, contractAddress, web3);
