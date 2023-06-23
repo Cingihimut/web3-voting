edcb6b59017c11ffdad9554911b7eb38a0f252a0
Muhammad Aris <mhdarisid@gmail.com>
2023-06-24 00:34:37 +0700

    Test voting button and deploy smart contract to sepolia ETH tesnet: Fix SetButtonAddrs

--- src/app/utils/web3.js
+++ src/app/utils/web3.js
@@ -2,23 +2,19 @@ import { Web3 } from "web3";
 
 let web3;
 export const connectWeb3 = async () => {
-  // Cek jika web3 sudah ada di browser
-  if (typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
-    try {
-      // Minta izin untuk mengakses akun pengguna
-      await window.ethereum.request({ method: "eth_requestAccounts" });
-      // Buat instance Web3 dengan provider dari Metamask atau wallet lainnya
-
-      web3 = new Web3(
-        new Web3.providers.HttpProvider(process.env.INFURA_API_KEY)
-      );
-      return web3;
-    } catch (error) {
-      console.error(error);
-      throw new Error("Gagal terhubung dengan provider Web3");
+  return new Promise(async (resolve, reject) => {
+    if (window.ethereum) {
+      try {
+        await window.ethereum.request({ method: "eth_requestAccounts" });
+        const web3 = new Web3(window.ethereum);
+        resolve(web3);
+      } catch (error) {
+        reject(error);
+      }
+    } else {
+      reject(new Error("Metamask tidak tersedia di browser"));
     }
-  } else {
-    throw new Error("Provider Web3 tidak ditemukan");
-  }
+  });
 };
+
 export { web3 };
